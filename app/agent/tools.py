46e885d17e38eba7de42d415524153664bb17892
nilsonjuniorr <nilson.junior@provafacilnaweb.com.br>
2026-04-22 16:51:12 -0300

    feat: add agent with create hero

--- app/agent/tools.py
+++ app/agent/tools.py
@@ -1,6 +1,5 @@
 from langchain_core.tools import tool
 from pydantic import BaseModel, Field
-from sqlalchemy.exc import IntegrityError
 from app.services.heroes import HeroService
 from app.schemas import SuperHeroSchema
 
